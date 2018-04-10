d1eff5ab5d94728166f1cfa78c802fd4fdbf5597
Mercury Carver <mercurycarver@gmail.com>
2018-04-10 12:31:28 -0400

    first commit

---
+++ FINDALLNUMBERS.java
@@ -0,0 +1,13 @@
+
+public class FINDALLNUMBERS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i =10;i<=500;i++) {
+	if((i%2!=0)&&(i%3!=0)&&(i%5!=0)) {
+		System.out.println(i);
+	}
+}
+	}
+
+}
